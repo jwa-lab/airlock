960061a6b788c985562e1a2e2313c00e81c33cff
Julien <julien.deporte@epitech.eu>
2021-07-21 16:07:01 +0200

    BSP-133: Authorization header given to NATS request.
    fix(natsToRestBridge.ts): Create a NATS header containing the HTTP authorization header if any.
    
    BSP-133: Conditional check.

--- src/middlewares/natsToRestBridge.ts
+++ src/middlewares/natsToRestBridge.ts
@@ -1,4 +1,4 @@
-import { ErrorCode, JSONCodec, NatsConnection } from "nats";
+import { ErrorCode, JSONCodec, NatsConnection, headers as natsHeaders } from "nats";
 import { NextFunction, Request, Response } from "express";
 import { BadRequestError, NotFoundRequestError } from "../lib/errors";
 
@@ -16,22 +16,27 @@ export default function restToNatsBridgeFactory(
         res: Response,
         next: NextFunction
     ): Promise<void> {
-        const { method, path, body, query, originalUrl } = req;
+        const { method, path, body, query, originalUrl, headers } = req;
         const subjectName = `${method}:${path
             .substring(1)
             .split("/")
             .join(".")}`;
+        const natsRequestHeaders = natsHeaders();
 
         console.log(`[AIRLOCK] Request on ${subjectName}`);
 
         try {
+            if (headers?.authorization) {
+                natsRequestHeaders.append('authorization', headers.authorization);
+            }
+
             const reply = await natsConnection.request(
                 subjectName,
                 jsonCodec.encode({
                     body,
                     query
                 }),
-                { timeout: 30000 }
+                { timeout: 30000, headers: natsRequestHeaders }
             );
 
             const response = jsonCodec.decode(reply.data) as PlatformResponse;
